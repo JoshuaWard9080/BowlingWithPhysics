387bdd69346261b3c8053ed74e411a05a707d9ad
JoshuaWard9080 <144284996+JoshuaWard9080@users.noreply.github.com>
2025-02-09 14:41:02 -0800

    Pins and Update Controls
    Added Pin prefabs and changed the controls to ensure the player cannot continuously roll the ball

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,17 +6,20 @@ public class BallController : MonoBehaviour
     [SerializeField] private float force;
     [SerializeField] private InputManager inputManager;
 
+    private bool isBallLaunched;
     private Rigidbody ballRB;
 
     void Start()
     {
         ballRB = GetComponent<Rigidbody>();
-
         inputManager.OnSpacePressed.AddListener(LaunchBall);
     }
 
     private void LaunchBall()
     {
+        if (isBallLaunched) return;
+
+        isBallLaunched = true;
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
